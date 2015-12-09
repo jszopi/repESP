3d9fe76e3502b47104dc68947f56409ce33328b9
Jan Szopinski <jszopi@users.noreply.github.com>
2015-12-09 15:26:46 +0000

    Fix code repetition between skim and filter funcs
    Also added exhaustive documentation of those functions and their helper.

--- field_comparison.py
+++ field_comparison.py
@@ -48,36 +48,110 @@ def _check_grids(field1, *fields):
 
 
 def filter_by_dist(exclusion_dist, dist, *fields, assign_val=None):
-    # Note the code repetition from the `skim' function. The logic is somewhat
-    # different here so I wasn't sure how I could generalize and decided that
-    # modularity was more important. Note that the arrays are traversed twice
-    # if both this function and `skim' are called consecutively.
-    _check_grids(dist, *fields)
-    if 'dist' not in dist.field_type:
-        raise TypeError("The field passed was of type '{0}' but one of the "
-                        "'dist' types was expected.".format(dist.field_type))
+    """Filter the datapoints in input fields by their distances.
 
-    fields = [field.values.copy() if type(field) is Field else field.copy() for
-              field in fields]
-    dist = dist.values.copy() if type(dist) is Field else dist.copy()
-    for dist_elem, *field_elems in np.nditer([dist] + fields,
-                                             op_flags=['readwrite']):
-        if dist_elem <= exclusion_dist:
-            dist_elem[...] = assign_val
-            for field_elem in field_elems:
-                field_elem[...] = assign_val
-    # Return all the input fields, including distance fields, as a list
-    return [dist] + fields
+    This function takes any number of field-like objects which describe
+    datapoints corresponding between the fields. The `dist` object specifies
+    their distances from something, e.g. an electron density isosurface. This
+    function will filter out the values of all datapoints, whose distances are
+    smaller than a threshold value, will be filtered or actually replaced by a
+    placeholder value `assign_val`. The input objects are not affected, as new
+    np.ndarrays are returned.
+
+
+    Parameters
+    ----------
+    exclusion_dist : float
+        Datapoints at distances smaller or equal this number will be filtered.
+    dist : Field or np.ndarray
+        The Field or ndarrays specifying distance of each datapoint. This
+        object also gets filtered.
+    *fields : Field or np.ndarray
+        The Fields or ndarrays containing the datapoints to be filtered by
+        distance.
+    assign_val : Optional
+        The datapoints within the exclusion distance will be replaced by this
+        value. Defaults to None.
+
+    Returns
+    -------
+    List[np.ndarray]
+        The filtered values of `dist` and `fields`.
+
+    """
+    condition = lambda elems: elems[0] <= exclusion_dist
+    fields = [dist] + list(fields)
+    return _iterate_fields(condition, assign_val, *fields)
 
 
 def skim(rand_skim, *fields, assign_val=None):
+    """Skim the number of datapoints in input fields.
+
+    This function takes any number of field-like objects which describe
+    datapoints corresponding between the fields. This function will randomly
+    remove some of the datapoints (the corresponding points between the
+    different fields) and retain a fraction of all datapoints given by the
+    `rand_skim` argument. The datapoints are not actually removed but replaced
+    with placeholder values `assign_val`. The input objects are not affected,
+    as new np.ndarrays are returned.
+
+    This function is useful for reducing the number of datapoints in a 3D plot,
+    whose interactivity or clarity would otherwise be affected.
+
+    Parameters
+    ----------
+    rand_skim : float
+        A number between 0 and 1. The fraction of datapoints to be retained.
+    *fields : Field or np.ndarray
+        The Fields or ndarrays containing the datapoints to be skimmed.
+    assign_val : Optional
+        The datapoints to be removed will be replaced by this value. Defaults
+        to None.
+
+    Returns
+    -------
+    List[np.ndarray]
+        The skimmed fields.
+
+    """
+    condition = lambda elems: random.random() > rand_skim
+    return _iterate_fields(condition, assign_val, *fields)
+
+
+def _iterate_fields(condition, assign_val, *fields):
+    """Iterate and remove corresponding elements of ndarrays
+
+    This function takes any number of field-like objects which describe
+    datapoints corresponding between the fields. It will remove the
+    corresponding datapoints from all the fields depending whether they satisfy
+    the passed `condition` function. The datapoints are not actually removed
+    but replaced with placeholder values `assign_val`. The input objects are
+    not affected, as new np.ndarrays are returned.
+
+    Parameters
+    ----------
+    condition : bool
+        A function which takes the list of corresponding elements in the
+        iterated fields and return a boolean value stating whether the list
+        satisfies the condition.
+    assign_val
+        The datapoints to be removed will be replaced by this value.
+    *fields : Field or np.ndarray
+        The fields containing the datapoints to be iterated.
+
+    Returns
+    -------
+    List[np.ndarray]
+        The transformed field values.
+
+    """
     _check_grids(*fields)
     fields = [field.values.copy() if type(field) is Field else field.copy() for
               field in fields]
     # Numpy array iteration with nditer has a convenient write mode, but it
     # would affect the input arrays, so copies were made earlier.
     for field_elems in np.nditer(fields, op_flags=['readwrite']):
-        if random.random() > rand_skim:
+        if condition(field_elems):
             for field_elem in field_elems:
                 field_elem[...] = assign_val
     return fields
