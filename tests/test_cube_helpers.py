c82a013ff7a570f6cf23326943f17940ea122e07
Jan Szopinski <jszopi@users.noreply.github.com>
2016-01-28 07:58:49 +0000

    Create the first few tests and a test cube
    The test cube was written by hand except for the electron density
    values, which were taken from water.

---
+++ tests/test_cube_helpers.py
@@ -0,0 +1,19 @@
+import unittest
+from repESP import cube_helpers
+
+# I'm not sure whether this should be here but it seems fine, it's only
+# evaluated once and will be available to all the TestCases in this module
+cube = cube_helpers.Cube("tests/test_mol_den.cub")
+
+
+class TestCube(unittest.TestCase):
+
+    def test_title(self):
+        self.assertEqual(cube.title,
+                         " Electron density from Total MP2 Density")
+
+    def test_atom_count(self):
+        self.assertEqual(cube.atom_count, 1)
+
+    def test_type(self):
+        self.assertEqual(cube.cube_type, 'ed')
