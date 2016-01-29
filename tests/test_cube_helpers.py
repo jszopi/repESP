727059455de7aec393e0ea45f99f60d64587873b
Jan Szopinski <jszopi@users.noreply.github.com>
2016-01-29 11:02:58 +0000

    Refactor variable names in test

--- tests/test_cube_helpers.py
+++ tests/test_cube_helpers.py
@@ -4,6 +4,8 @@ from repESP import cube_helpers
 # I'm not sure whether this should be here but it seems fine, it's only
 # evaluated once and will be available to all the TestCases in this module
 cube = cube_helpers.Cube("tests/test_mol_den.cub")
+atom = cube.molecule[0]
+grid = cube.field.grid
 
 
 class TestCube(my_unittest.TestCase):
@@ -22,17 +24,16 @@ class TestCube(my_unittest.TestCase):
 class TestGrid(my_unittest.TestCase):
 
     def test_origin_coords(self):
-        self.assertListAlmostEqual(cube.field.grid.origin_coords,
-                                   [0.1, 0.2, 0.3])
+        self.assertListAlmostEqual(grid.origin_coords, [0.1, 0.2, 0.3])
 
 
 class TestMolecule(my_unittest.TestCase):
 
     def test_label(self):
-        self.assertEqual(cube.molecule[0].label, 1)
+        self.assertEqual(atom.label, 1)
 
     def test_atomic_no(self):
-        self.assertEqual(cube.molecule[0].atomic_no, 1)
+        self.assertEqual(atom.atomic_no, 1)
 
     def test_coords(self):
-        self.assertListAlmostEqual(cube.molecule[0].coords, [0.1, 0.2, 0.4])
+        self.assertListAlmostEqual(atom.coords, [0.1, 0.2, 0.4])
