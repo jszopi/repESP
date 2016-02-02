01dd3a7f131754f1ce175a482f926c3396d9862b
Jan Szopinski <jszopi@users.noreply.github.com>
2016-02-02 16:11:49 +0000

    New Molecule method: extract QTAIM basins from bader
    Also started using reStructuredText in documentation and comments.

--- repESP/cube_helpers.py
+++ repESP/cube_helpers.py
@@ -2,6 +2,8 @@ import numpy as np
 from operator import attrgetter
 from scipy.ndimage.morphology import distance_transform_edt as scipy_edt
 from scipy.spatial.distance import euclidean
+import glob
+import os
 
 AXES = ['x', 'y', 'z']
 
@@ -236,6 +238,70 @@ class Molecule(list):
                 min_dist = dist
                 min_atom = atom.label
         return (min_atom, min_dist)
+
+    def extract_qtaim_basins(self, grid, path):
+        """Extract QTAIM basins from Henkelman group's ``bader`` program
+
+        The ``bader`` command needed to generate input cube files is::
+
+            bader -p all_atom -vac off density.cube
+
+        Assigning low density points to vacuum needs to be switched off in
+        order to allow the basins to extend to infinty.
+
+        This method returns a field with atomic labels indicating which basin
+        each point belongs to.
+        """
+        output_files = glob.glob(path + 'BvAt*.cube')
+        expected = [path + 'BvAt{0:04}.cube'.format(i+1) for i in
+                    range(len(self))]
+
+        if sorted(output_files) != expected:
+            if len(output_files) == 0:
+                msg = "No ``bader`` output cube files found!"
+            else:
+                for output_file, expected_file in zip(output_files, expected):
+                    if output_file != expected_file:
+                        msg += "Missing expected ``bader`` output cube file: "
+                        msg += os.path.basename(expected)
+                        break
+
+            raise InputFormatError(msg + " To generate the files use the "
+                                   "command: ``bader -p all_atom -vac off "
+                                   "density.cube``")
+
+        cubes = [Cube(expected_file) for expected_file in expected]
+        # Compare grids with that provided. TODO: Would be better to use the
+        # function field_comparison._check_grids, but can't import that module
+        # here and won't be able to pass a special message. All that requires
+        # some refactoring but is a sensible thing to do.
+        for i, cube in enumerate(cubes):
+            if cube.field.grid != grid:
+                raise GridError("The grid of `bader' cube number {0} is "
+                                "different from that of the molecule "
+                                "requesting extraction.".format(i+1))
+
+        result = []
+        # Iterate all the cubes element-wise and produce a field with atomic
+        # labels indicating which basin each point belongs to.
+        # (This probably isn't the numpy way of doing this. It operates on
+        # iterators though, so should be memory efficient.)
+        for point in zip(*[cube.field.values.flat for cube in cubes]):
+            point_bool = [True if elem else False for elem in point]
+            if sum(point_bool) == 0:
+                raise InputFormatError("Found point not assigned to any atom "
+                                       "by the ``bader`` program. Maybe the "
+                                       "``-vac off`` option was not set?")
+            elif sum(point_bool) > 1:
+                raise InputFormatError("Found point assigned to many atoms "
+                                       "by the ``bader`` program. Possible "
+                                       "numerical inconsitency in algorithm.")
+            result.append(point_bool.index(True)+1)
+
+        result = np.array(result)
+        result.resize(self.parent_cube.field.grid.points_on_axes)
+        return Field(result, self.parent_cube.field.grid, 'parent_atom',
+                     ['qtaim'])
 
 
 class Field(object):
