ec9d4c5848803d82797ff997ac415b214304a380
Jan Szopinski <jszopi@users.noreply.github.com>
2016-03-07 17:00:16 +0000

    Rectify the issue with fixing shell thickness
    I realized that IOp43 ('increment between shells') likely refers to
    multiples of van der Waals radii, just like in the MK paper, rather than
    a value in angstroms. However, investigating the distances on my 2D plot
    of methane showed, that the value used for hydrogen is unusual. I did
    some research and described it extensively in the docstrings. A formula
    in the code had to be updated to account for the vdW radius factor in
    the increment.

--- scripts/fit_points.py
+++ scripts/fit_points.py
@@ -73,33 +73,81 @@ class IOpCalcSet(object):
 
     def __init__(self, iop41=None, iop42=None, iop43=None,
                  thickness=None, thick_round='closest'):
-        """Creatre object for creating sets of IOp values
+        """Create object for creating sets of IOp values
 
-        Parameters `iop41`, `iop42` and `iop43` can be specified as `None`, 0,
-        an integer or a list of integers. If `None` or 0 is given, the default
-        value will be looked-up for `iop41` and `iop42`; for `iop43` 0 will be
-        passed, as it requests an option which cannot be specified otherwise.
-        At least one of the parameters must be in the form of a list, as its
-        length will be used as the requested length of other lists. If more
-        than one of the three parameters is given as a list, they must be of
-        equal lengths. If a parameter is given as an integer, a list will be
+        .. This is not a finished comprehensive docstring. In partiular, the
+            meaning of the IOps is not explained. I have only polished the bits
+            pertaining to the tricky ``thickness`` calculation.
+
+        Parameters ``iop41``, ``iop42`` and ``iop43`` can be specified as
+        ``None``, 0, an integer or a list of integers. If ``None`` or 0 is
+        given, the default value will be looked-up for ``iop41`` (4) and
+        ``iop42`` (1); for ``iop43`` 0 will be passed, as it requests an option
+        which cannot be specified otherwise (Gaussian's own formula, see
+        :ref:`below <gaussian_formula>`).
+
+        At least one of the parameters must be in the form of a list,
+        as its length will be used as the requested length of other lists. If
+        more than one of the three parameters is given as a list, they must be
+        of equal lengths. If a parameter is given as an integer, a list will be
         created consisting of the same element repeated.
 
-        The default value of `iop43` requests Gaussian's own formula
-        calculating it based on the number of layers (`iop41`). However, to
-        keep the overall thickness of the set of points, another formula must
-        be used. This an be achieved by leaving `iop43` as None and specifying
-        the desired thickness instead. To use the default thickness (resulting
-        from Gaussian's combination of default number of layers and increment
-        formula), specify a thickness of:
+        The default value of ``iop43`` requests Gaussian's own formula, which
+        calculates the shell interval (:math:`d`) based on the number of layers
+        (``iop41``, :math:`n_l`):
 
-        .. math:: (#l-1)*0.4/sqrt(#l) = (4-1)*0.4/sqrt(4) = 0.3
+        .. _gaussian_formula:
+        .. math::
 
-        However, since `iop43` can only be an integer (which is then
-        multiplied by 0.01 to obtain the actual value of the increment), it
-        will not be possible to keep a constant value of thickness. The default
-        method of rounding the increment is 'closest' but you can also select
-        'up' or 'down' to obtain a range on your plot.
+            d = \\frac{0.4}{\sqrt{n_l}}
+
+        For the default number of shells (4), the interval is 0.2. Note that,
+        according to the MK method, the interval between shells is specified in
+        multiples of van der Waals radii.
+
+        .. note:: The original MK paper (Singh1984) cites the MS program by M.
+            Connolly for the data on van der Waals (vdW) radii. However, as I
+            found when investigating the fitting points around methane, the
+            value used by Gaussian for the vdW radius of hydrogen is 1.53 Å,
+            which is far off the usual value around 1.2 Å. I suppose that the
+            carbon radius used in the Conolly program (`link`_) may have been
+            mistakenly used.
+
+            .. _link: http://sites.bmb.uga.edu/wampler/8200/size/MS-radii.html
+
+            .. The parent link is:
+                http://sites.bmb.uga.edu/wampler/8200/size/connolly.html#SURF
+
+            Interestingly, my investigation of methane also showed that the
+            radius used to exclude the inner points (i.e. calculate the first
+            shell at 1.4 vdW radius) is different. It appears to be around 1.18
+            Å, so more in line with the usual vdW radius for hydrogen.
+
+        One may also wish to keep the overall thickness of the set of points
+        constant. This cannot be achieved through IOps, since the increment
+        between MK shells is in multiples of van der Waals radii, so is
+        variable depending on the region of the molecule. Therefore thickness
+        can be set assuming a single van der Waals radius, that of hydrogen.
+        This can be achieved by leaving ``iop43`` as None and specifying the
+        desired thickness instead with the ``'thickness'`` keyword. Gaussian's
+        Merz-Singh-Kollman radius for hydrogen (1.53 Å) will then be used. The
+        thickness resulting from Gaussian's combination of default number of
+        layers (4) and the increment calculated with the aforementioned default
+        formula is:
+
+        .. math::
+
+           \\newcommand{angstrom}{\\mathrm{\\unicode[serif]{xC5}}}
+
+           1.53 \\angstrom \\times d (n_l-1) = 1.53 \\angstrom \\times (4 - 1)
+           \\frac{0.4} {\sqrt{4}} = 0.918 \\angstrom
+
+        Since ``iop43`` can only be an integer (which is then multiplied by
+        0.01 to obtain the actual value of the increment), it will not be
+        possible to keep exactly a constant value of thickness. The default
+        method of rounding the increment is ``'closest'`` but you can also
+        select ``'up'`` or ``'down'`` to obtain a range on your plot.
+
         """
         if thickness is not None and iop43 is not None:
             raise ValueError("Option `thickness` cannot be set if `iop43` is "
@@ -128,8 +176,8 @@ class IOpCalcSet(object):
 
         if self.thickness is not None:
             # Calculate intervals according to thickness
-            result[-1] = [self.thick_round(100*self.thickness/(layers_count-1))
-                          for layers_count in result[0]]
+            result[-1] = [self.thick_round(100*self.thickness/1.53/(
+                          layers_count-1)) for layers_count in result[0]]
 
         self._check_result(result)
         return result
