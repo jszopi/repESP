cacf9fdac73410550d384133e68102ffeece8ab3
Jan Szopinski <jszopi@users.noreply.github.com>
2016-06-17 19:41:08 +0100

    Create a parent parser for scripts
    This parent parser contains two arguments: input charge type and file.

---
+++ scripts/parent_parser.py
@@ -0,0 +1,36 @@
+import argparse
+
+
+def input_type(input_charge_type):
+    if input_charge_type == 'list':
+        result = 'qout'
+    elif input_charge_type == 'aim':
+        result = 'sumviz'
+    else:
+        result = 'log'
+    return result
+
+
+parent_parser = argparse.ArgumentParser(
+    add_help=False)
+
+charge_choices = ['list', 'mulliken', 'nbo', 'aim', 'mk', 'chelp', 'chelpg',
+                  'hly']
+charge_choices_text = "'" + "', '".join(charge_choices[1:-1]) + "' and '" + \
+                      charge_choices[-1] + "'."
+parent_parser.add_argument(
+    "input_charge_type",
+    help="type of input charges. To extract charges from a text file, use the "
+    "'list' option. Other available options are: " + charge_choices_text,
+    choices=charge_choices, metavar="INPUT_CHARGE_TYPE")
+
+parent_parser.add_argument(
+    "input_charge_file",
+    help="""input file to extract charges from. This can be a Gaussian .log,
+    AIMAll .sumviz or a text file. The appropriate format for the text file is
+    used throughout this suite of scripts and is very simple:
+
+    1. Values should be space-separated
+    2. Atom order should follow that in the template cube file
+    3. Line breaks are not significant""",
+    metavar="INPUT_CHARGE_FILE")
--- scripts/rep_esp.py
+++ scripts/rep_esp.py
@@ -4,6 +4,7 @@ from repESP import cube_helpers, charges
 from repESP.rep_esp import calc_grid_field
 
 import argparse
+import parent_parser
 import os
 
 help_description = """
@@ -18,6 +19,7 @@ help_description = """
 # TODO: give the name of the appropriate script once it's been written
 
 parser = argparse.ArgumentParser(
+    parents=[parent_parser.parent_parser],
     description=help_description,
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
@@ -25,27 +27,6 @@ parser.add_argument("template_cube",
                     help="template cube file (content doesn't matter, only the"
                     " grid and atomic coordinates are used)",
                     metavar="TEMPLATE_CUBE")
-
-charge_choices = ['list', 'mulliken', 'nbo', 'aim', 'mk', 'chelp', 'chelpg',
-                  'hly']
-charge_choices_text = "'" + "', '".join(charge_choices[1:-1]) + "' and '" + \
-                      charge_choices[-1] + "'."
-parser.add_argument(
-    "input_charge_type",
-    help="type of input charges. To extract charges from a text file, use the "
-    "'list' option. Other available options are: " + charge_choices_text,
-    choices=charge_choices, metavar="INPUT_CHARGE_TYPE")
-
-parser.add_argument(
-    "input_charge_file",
-    help="""input file to extract charges from. This can be a Gaussian .log,
-    AIMAll .sumviz or a text file. The appropriate format for the text file is
-    used throughout this suite of scripts and is very simple:
-
-    1. Values should be space-separated
-    2. Atom order should follow that in the template cube file
-    3. Line breaks are not significant""",
-    metavar="INPUT_CHARGE_FILE")
 
 parser.add_argument("-o", "--output",
                     help="output file name",
@@ -59,13 +40,7 @@ if os.path.exists(args.output):
 template_cube = cube_helpers.Cube(args.template_cube)
 molecule = template_cube.molecule
 
-if args.input_charge_type == 'list':
-    input_type = 'qout'
-elif args.input_charge_type == 'aim':
-    input_type = 'sumviz'
-else:
-    input_type = 'log'
-
+input_type = parent_parser.input_type(args.input_charge_type)
 charges._get_charges(args.input_charge_type, args.input_charge_file,
                      input_type, molecule)
 
