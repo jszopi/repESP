e2baa383510666aaad2a7db1274ce0f9d08fc4e3
Jan Szopinski <jszopi@gmail.com>
2018-09-06 01:24:13 +0100

    Type fixes to calc_fields.py

--- repESP/calc_fields.py
+++ repESP/calc_fields.py
@@ -1,28 +1,30 @@
 from .types import Charges, Coords, Dist, Field, Mesh, Molecule
 
 from scipy.spatial.distance import euclidean
-from typing import List, Tuple
+from typing import List, NewType, Optional, Tuple
 
 
-def _esp_from_charges_at_point(coords: Coords, charges: Charges) -> FloatFieldValue:
+Esp = NewType("Esp", float)  # Electrostatic potential [atomic units]
 
-    return sum(
-        # NOTE: I've removed the conversion from angstrom to bohr
-        FloatFieldValue(charge/(euclidean(coords, atom.coords)))
+
+def _esp_from_charges_at_point(coords: Coords, charges: Charges) -> Esp:
+
+    return Esp(sum(
+        charge/(euclidean(coords, atom.coords))
         for atom, charge in zip(charges.molecule.atoms, charges.values)
-    )
+    ))
 
 
-def esp_from_charges(mesh: Mesh, charges: Charges) -> Field[FloatFieldValue]:
+def esp_from_charges(mesh: Mesh, charges: Charges) -> Field[Esp]:
     """Calculate ESP value at given point due to charges on atoms"""
     return mesh.calc_field(
         lambda coords: _esp_from_charges_at_point(coords, charges)
     )
 
 
-def _voronoi_at_point(coords: Coords, molecule: Molecule) -> Tuple[int, Dist]:
+def _voronoi_at_point(coords: Coords, molecule: Molecule) -> Tuple[Optional[int], Dist]:
     """For a given point, find the closest atom and its distance"""
-    min_dist = float('inf')
+    min_dist = Dist(float('inf'))
     min_atom = None
     for atom_index, atom in enumerate(molecule.atoms):
         dist = euclidean(coords, atom.coords)
@@ -32,7 +34,7 @@ def _voronoi_at_point(coords: Coords, molecule: Molecule) -> Tuple[int, Dist]:
     return (min_atom, min_dist)
 
 
-def voronoi(mesh: Mesh, molecule: Molecule) -> Field[Dist]:
+def voronoi(mesh: Mesh, molecule: Molecule) -> Field[Tuple[Optional[int], Dist]]:
     # Voronoi means closest-atom in molecular partitioning lingo
     return mesh.calc_field(
         lambda coords: _voronoi_at_point(coords, molecule)
