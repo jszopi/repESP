fe112ec992e5ebd1878ba4f73651dc920c68ae25
Jan Szopinski <jszopi@gmail.com>
2018-09-08 13:12:23 +0100

    Add cube_parsers with tests

---
+++ repESP/cube_parsers.py
@@ -0,0 +1,136 @@
+from .types import Atom, Ed, Esp, Coords, Field, GridMesh, Molecule
+from .types import make_coords, make_ed, make_esp
+from .exceptions import InputFormatError
+
+from enum import Enum
+from typing import Any, Callable, Generic, List, Mapping, NamedTuple, NewType, Optional, TextIO, Tuple, TypeVar, Union
+
+
+FieldValue = TypeVar('FieldValue')
+
+
+class CubeInfo(NamedTuple):
+    input_line: str
+    title_line: str
+
+
+class Cube(NamedTuple, Generic[FieldValue]):
+    cube_info: CubeInfo
+    molecule: Molecule
+    field: Field[FieldValue]
+
+
+class _GridPrelude(NamedTuple):
+    atom_count: int
+    origin: Coords
+    nval: float
+
+
+def _parse_grid_prelude(line: str) -> _GridPrelude:
+    line_split = line.split()
+    if len(line_split) in (4, 5) :
+        atom_count, *origin_coords = line_split[:4]
+        nval = line_split[4] if len(line_split) == 5 else "1"
+    else:
+        raise InputFormatError(
+            f"Cube file incorrectly formatted! Expected four or five fields "
+            "(atom count, 3*origin coordinates, [NVal]) on line 3, found "
+            "{len(line_split)} fields."
+        )
+
+    return _GridPrelude(
+        int(atom_count),
+        make_coords(*origin_coords),
+        float(nval)
+    )
+
+
+def _parse_grid(origin: Coords, lines: List[str]) -> GridMesh:
+
+    def parse_axis(line: str) -> GridMesh.Axis:
+        point_count, *vector_components = line.split()
+        return GridMesh.Axis(
+            make_coords(*vector_components),
+            int(point_count)
+        )
+
+    assert len(lines) == 3
+
+    return GridMesh(
+        origin,
+        GridMesh.Axes(tuple(  # type: ignore # (asserted len is 3)
+            parse_axis(line) for line in lines
+        ))
+    )
+
+
+def _parse_atom(line: str) -> Atom:
+    identity, _cube_charge, *coords = line.split()
+    return Atom(int(identity), make_coords(*coords))
+
+
+def parse_cube(
+    f: TextIO,
+    make_value: Callable[[CubeInfo, str], FieldValue]
+) -> 'Cube[FieldValue]':
+    # Assumption: coordinates in bohr
+
+    get_line = lambda: f.readline().rstrip('\n')
+
+    # Lines 1-2
+    cube_info = CubeInfo(input_line=get_line(), title_line=get_line())
+
+    # Line 3
+    atom_count, origin, nval = _parse_grid_prelude(get_line())
+
+    if float(nval) != 1:
+        # I don't know what NVal means, haven't seen it to be different than 1.
+        raise InputFormatError("NVal is different than 1.")
+
+    # Lines 4-6
+    grid = _parse_grid(origin, [get_line() for i in range(3)])
+
+    # Molecule
+    molecule = Molecule(list(_parse_atom(get_line()) for i in range(atom_count)))
+
+    # Field values
+    value_ctor = lambda x: make_value(cube_info, x)
+    # TODO: this may be unfeasible for very large cubes
+    values = [value_ctor(x) for x in f.read().split()]
+
+    return Cube(
+        cube_info,
+        molecule,
+        Field(grid, values)
+    )
+
+
+def _parse_cube_by_title_common(
+    expected_title_start: str,
+    value_ctor: Callable[[str], FieldValue],
+    verify_title: bool
+) -> Callable[[CubeInfo, str], FieldValue]:
+
+    def make_value(cube_info: CubeInfo, value: str) -> FieldValue:
+        check_title = lambda title: title.startswith(expected_title_start)
+        if verify_title and not check_title(cube_info.title_line):
+            raise InputFormatError(
+                f'Title of cube file does not start with "{expected_title_start}".'
+            )
+        return value_ctor(value)
+
+    return make_value
+
+
+def parse_esp_cube(f: TextIO, verify_title=True) -> 'Cube[Esp]':
+    return parse_cube(
+        f,
+        _parse_cube_by_title_common(" Electrostatic potential", make_esp, verify_title)
+    )
+
+
+def parse_ed_cube(f: TextIO, verify_title=True) -> 'Cube[Ed]':
+    return parse_cube(
+        f,
+        _parse_cube_by_title_common(" Electron density", make_ed, verify_title)
+    )
---
+++ tests/test_cube_parsers.py
@@ -0,0 +1,69 @@
+from repESP.types import *
+from repESP.cube_parsers import parse_ed_cube
+
+from my_unittest import TestCase, make_coords
+
+class TestCubeParser(TestCase):
+
+    def setUp(self) -> None:
+        with open("tests/test_mol_den.cub", 'r') as f:
+            self.cube = parse_ed_cube(f)
+
+    def test_cube_data(self) -> None:
+        self.assertEqual(
+            self.cube.cube_info.input_line,
+            " Test molecule density=mp2"
+        )
+        self.assertEqual(
+            self.cube.cube_info.title_line,
+            " Electron density from Total MP2 Density"
+        )
+
+    def test_grid(self) -> None:
+
+        self.assertListsAlmostEqual(
+            [0.1, 0.2, 0.3],
+            self.cube.field.mesh._origin  # type: ignore # (accessing subclass attribute)
+        )
+
+        self.assertListsAlmostEqual(
+            [0.1, 0.2, 0.3],
+            self.cube.field.mesh._origin  # type: ignore # (accessing subclass attribute)
+        )
+
+        expected_axis_vecs = [
+            make_coords(0.2, 0, 0),
+            make_coords(0, 0.3, 0),
+            make_coords(0, 0, 0.4)
+        ]
+
+        for axis, expected_axis_vec in zip(
+            self.cube.field.mesh._axes,  # type: ignore # (accessing subclass attribute)
+            expected_axis_vecs
+        ):
+            self.assertEqual(axis.point_count, 3)
+            self.assertListsAlmostEqual(axis.vector, expected_axis_vec)
+
+    def test_molecule(self) -> None:
+
+        self.assertEqual(len(self.cube.molecule), 1)
+        self.assertEqual(self.cube.molecule.atoms[0].identity, 1)
+        self.assertEqual(self.cube.molecule.atoms[0].coords, make_coords(0.1, 0.2, 0.4))
+
+    def test_field_values(self) -> None:
+
+        values = [
+            1.36229e-08, 2.28426e-08, 3.69340e-08, 5.75781e-08, 8.65339e-08,
+            1.25366e-07, 1.75080e-07, 2.35719e-07, 3.06022e-07, 3.83225e-07,
+            4.63120e-07, 5.40378e-07, 6.09116e-07, 6.63619e-07, 6.99085e-07,
+            7.12274e-07, 7.01958e-07, 6.69101e-07, 6.16729e-07, 5.49515e-07,
+            4.73127e-07, 3.93467e-07, 3.15935e-07, 2.44838e-07, 1.83067e-07,
+            1.32029e-07, 9.18219e-08
+        ]
+
+        ed_values = [Ed(value) for value in values]
+
+        self.assertEqual(
+            ed_values,
+            self.cube.field.values
+        )
