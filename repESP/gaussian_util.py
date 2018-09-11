cd7a25f82c17835addc41220515bca6518339d2c
Jan Szopinski <jszopi@gmail.com>
2018-09-11 11:13:45 +0100

    Refactor gaussian_util.py to decouple functions

--- repESP/gaussian_util.py
+++ repESP/gaussian_util.py
@@ -2,7 +2,9 @@ from .charges import Charge, ChargeType, make_charge
 from .exceptions import InputFormatError
 from .types import Atom, Coords, Molecule
 
-from typing import List, Optional, TextIO, Tuple
+from dataclasses import dataclass
+import re
+from typing import List, Mapping, Optional, TextIO, Tuple
 
 
 handled_charge_types = [
@@ -21,22 +23,18 @@ esp_charge_types = [
     ChargeType.HLY,
 ]
 
-esp_charge_identifier_line = {
-    ' Merz-Kollman atomic radii used.': ChargeType.MK,
-    ' Francl (CHELP) atomic radii used.': ChargeType.CHELP,
-    ' Breneman (CHELPG) radii used.': ChargeType.CHELPG,
-    ' Generate Potential Derived Charges using the Hu-Lu-Yang model:': ChargeType.HLY,
+charge_type_to_identifier_line: Mapping[ChargeType, str] = {
+    ChargeType.MULLIKEN: " Mulliken charges:",
+    ChargeType.MK: " Merz-Kollman atomic radii used.",
+    ChargeType.CHELP: " Francl (CHELP) atomic radii used.",
+    ChargeType.CHELPG: " Breneman (CHELPG) radii used.",
+    ChargeType.HLY: " Generate Potential Derived Charges using the Hu-Lu-Yang model",
+    ChargeType.NPA: " Summary of Natural Population Analysis:                  "
 }
 
-esp_charges_section_header = ' Charges from ESP fit,'
-
-section_headers = {
-    ChargeType.MULLIKEN: ' Mulliken charges:',
-    ChargeType.MK: esp_charges_section_header,
-    ChargeType.CHELP: esp_charges_section_header,
-    ChargeType.CHELPG: esp_charges_section_header,
-    ChargeType.HLY: esp_charges_section_header,
-    ChargeType.NPA: ' Summary of Natural Population Analysis:'
+identifier_line_to_charge_type = {
+    v: k
+    for k, v in charge_type_to_identifier_line.items()
 }
 
 
@@ -72,126 +70,193 @@ def get_charges_from_log(
 
     Returns
     -------
-    List[float]
+    List[Charge]
         List of charges in order of occurrence in output file.
 
     Raises
     ------
     InputFormatError
+        Raised when parsing of the input file fails.
+    IndexError
+        Raised whenn the requested occurence of the charges section cannot
+        be found could not be found in the output file.
+    NotImplementedError
+        Raised when finding and parsing the charges section of the requested
+        charge_type is not implemented.
     """
+    return _get_charges_section_from_log(f, charge_type, verify_against, occurrence).charges
+
+
+@dataclass
+class _ChargesSectionData:
+    charge_type: ChargeType
+    charges: List[Charge]
+
+
+def _get_charges_section_from_log(
+    f: TextIO,
+    charge_type: ChargeType,
+    verify_against: Optional[Molecule],
+    occurrence: int
+) -> _ChargesSectionData:
 
     if charge_type not in handled_charge_types:
         raise InputFormatError(
             f"Extracting charge type {charge_type} from Gaussian output file is not supported."
         )
 
-    charge_section = _get_charge_section(f, charge_type, occurrence)
-    charges = _parse_charges_section(charge_type, charge_section)
+    charges_sections = _get_charges_sections(f, charge_type)
 
-    if verify_against is not None:
-        if len(verify_against.atoms) != len(charges):
-            raise InputFormatError(
-                "Charges from log file failed verification against given molecule."
-            )
+    parsed_charges_sections = [
+        _parse_charges_section(charges_section, charge_type)
+        for charges_section in charges_sections
+    ]
 
-    return charges
-
-
-def _get_charge_section(
-    f: TextIO,
-    charge_type: ChargeType,
-    occurrence: int=-1
-) -> List[str]:
-    """Go to the selected occurrence of input about charges in a Gaussian output file.
-
-    The function includes extra logic to ensure that, if an ESP charge type was
-    requested, all ESP charge sections were recognized correctly. This is meant
-    as a safeguard in case the parsing fails without other symptoms.
-    """
-    current_offset = 0
-    charge_section_offsets = []
-    esp_types = []
-
-    for line in f:
-        current_offset += len(line)
-        line = line.rstrip('\n')
-
-        # All ESP charges are added here, as they cannot be distinguished just
-        # by the header
-        if section_headers[charge_type] in line.rstrip():
-            charge_section_offsets.append(current_offset)
-        # The information about the type of ESP charges is gathered separately
-        if charge_type in esp_charge_types and line in esp_charge_identifier_line:
-            esp_types.append(esp_charge_identifier_line[line])
-
-    if charge_type in esp_charge_types:
-        # Verify if all ESP charge output has been recognized correctly
-        if len(esp_types) != len(charge_section_offsets):
-            raise InputFormatError('Information about the type of some '
-                                   'ESP charges was not recognized.')
-        # Filter only the requested ESP charge type
-        charge_section_offsets = [elem for i, elem in enumerate(charge_section_offsets) if
-                  esp_types[i] == charge_type]
-
-    if not charge_section_offsets:
-        raise InputFormatError("Output about charge type '{0}' not found."
-                               .format(charge_type))
+    charges_sections_of_correct_type = [
+        charges_section
+        for charges_section in parsed_charges_sections
+        if charges_section.charge_type == charge_type
+    ]
 
     try:
-        f.seek(charge_section_offsets[occurrence])
+        selected_charges_section = charges_sections_of_correct_type[occurrence]
     except IndexError:
         raise IndexError(
-            "Cannot find occurrence '{0}' in a list of recognized pieces of "
-            "output about charges, which length is {1}.".format(
-                occurrence,
-                len(charge_section_offsets)
-            )
+            f"Cannot find occurrence {occurrence} in a list of recognized charges "
+            "sections in the output. Check if the program that produced the output "
+            "finished without errors. If you're sure that the charge section "
+            "should appear at least the specified number of times, please "
+            "submit a bug report attaching the file that failed parsing."
         )
 
-    # Skip unnecessary lines
-    lines_count = 1
-    if charge_type == ChargeType.NPA:
-        lines_count = 5
-    if charge_type in esp_charge_types:
-        lines_count = 2
+    if not _verify_charges_section(selected_charges_section, verify_against):
+        raise InputFormatError(
+            "Charges from log file failed verification against given molecule."
+        )
 
-    for counter in range(lines_count):
-        f.readline()
+    return selected_charges_section
 
-    # Read charge section until termination line
-    charge_lines: List[str] = []
+
+def _verify_charges_section(
+    charges_section: _ChargesSectionData,
+    verify_against: Optional[Molecule]
+) -> bool:
+    # TODO: This could be extended to check atom identities if those get parsed
+    if verify_against is None:
+        return True
+    else:
+        return len(verify_against.atoms) == len(charges_section.charges)
+
+
+def _get_charges_sections(
+    f: TextIO,
+    charge_type: ChargeType
+) -> List[List[str]]:
+    """Extract all charges sections which *may* be of the given type
+
+    Further verification of charge type is necessary based on parsing the section.
+    """
+    charges_sections: List[List[str]] = []
+    current_section: Optional[List[str]] = None
     for line in f:
-        if _is_section_termination(line, charge_type):
+        line = line.rstrip('\n')
+        if _is_section_start(line, charge_type):
+            if current_section is not None:
+                raise InputFormatError(
+                    "Encountered start of new charge section start while "
+                    "parsing a charge section. Please submit a bug report "
+                    "attaching the input file that failed parsing."
+                )
+            current_section = []
+
+        if current_section is not None:
+            current_section.append(line)
+            # Section end lines are less generic, hence we're only checking for
+            # them when inside a section.
+            if _is_section_end(line, charge_type):
+                charges_sections.append(current_section)
+                current_section = None
+
+    return charges_sections
+
+
+def _parse_charges_section(section: List[str], charge_type: ChargeType) -> _ChargesSectionData:
+
+    if charge_type in esp_charge_types:
+        return _parse_esp_charge_section(section)
+    elif charge_type == ChargeType.NPA:
+        charges = []
+        for line in section[6:-1]:
+            _symbol, _label, charge, *_other = line.split()
+            charges.append(make_charge(charge))
+        return _ChargesSectionData(
+            ChargeType.NPA,
+            charges
+        )
+    elif charge_type == ChargeType.MULLIKEN:
+        charges = []
+        for line in section[2:-1]:
+            _label, _symbol, charge = line.split()
+            charges.append(make_charge(charge))
+        return _ChargesSectionData(
+            ChargeType.MULLIKEN,
+            charges
+        )
+    else:
+        raise NotImplementedError(
+            f"Parsing of charge section for charge type {charge_type} is not implemented."
+        )
+
+
+def _parse_esp_charge_section(section: List[str]) -> _ChargesSectionData:
+
+    try:
+        charge_type = identifier_line_to_charge_type[section[0]]
+    except KeyError:
+        # This shouldn't happen as the start line already matched in _is_section_start
+        raise InputFormatError(
+            "Failed parsing first line of ESP charges section. Please submit "
+            " a bug report attaching the input file that failed parsing."
+        )
+
+
+    for i, line in enumerate(section):
+        if line.startswith(" Charges from ESP fit,"):
             break
-        charge_lines.append(line)
 
-    return charge_lines
+    charges = []
+    for line in section[i+3:]:
+        if _is_section_end(line, charge_type):
+            break
+        _label, _symbol, charge = line.split()
+        charges.append(make_charge(charge))
+
+    return _ChargesSectionData(
+        charge_type,
+        charges,
+    )
 
 
-def _is_section_termination(line: str, charge_type: ChargeType) -> bool:
+def _is_section_start(line: str, charge_type: ChargeType) -> bool:
 
-    if charge_type == ChargeType.NPA:
+    try:
+        return line == charge_type_to_identifier_line[charge_type]
+    except KeyError:
+        raise NotImplementedError(
+            f"Detection of start line for charge type {charge_type} is not implemented."
+        )
+
+
+def _is_section_end(line: str, charge_type: ChargeType) -> bool:
+
+    if charge_type in esp_charge_types or charge_type == ChargeType.MULLIKEN:
+        # TODO: This will not work with IOp(6/50=1), where all I know is that
+        # the termination line starts with ' Charges' but that yields false
+        # positives, so I have to find out a more specific regex.
+        return line.startswith(' Sum of ')
+    elif charge_type == ChargeType.NPA:
         return line.startswith(' =======')
     else:
-        # The latter happens when IOp(6/50=1) is requested
-        return line.startswith(' Sum of ') or line.startswith(' Charges')
-
-
-def _parse_charge_line(line: str, charge_type: ChargeType) -> Tuple[int, str, float]:
-    if charge_type == ChargeType.NPA:
-        symbol, label, charge, *other = line.split()
-    else:
-        label, symbol, charge = line.split()
-    # TODO: The labels and element symbols could be used to further verify
-    # against the passed molecule
-    return int(label), symbol, float(charge)
-
-
-def _parse_charges_section(
-    charge_type: ChargeType,
-    charge_section: List[str]
-) -> List[Charge]:
-    return [
-        make_charge(_parse_charge_line(line, charge_type)[2])
-        for line in charge_section
-    ]
+        raise NotImplementedError(
+            f"Detection of termination line for charge type {charge_type} is not implemented."
+        )
