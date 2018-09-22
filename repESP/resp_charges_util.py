182c08916854804b9b07b5b7365239e6fb90849b
Jan Szopinski <jszopi@gmail.com>
2018-09-22 19:29:37 +0100

    Add resp charge format parser

---
+++ repESP/resp_charges_util.py
@@ -0,0 +1,21 @@
+from .charges import Charge, make_charge
+
+from fortranformat import FortranRecordWriter as FW, FortranRecordReader as FR
+from functools import reduce
+from operator import add
+from typing import List, TextIO
+
+
+def parse_resp_charges(f: TextIO) -> List[Charge]:
+    formatter = FR("8F10.6")
+    return list(map(
+        make_charge,
+        filter(
+            lambda elem: elem is not None,
+            reduce(add, [formatter.read(line) for line in f], [])
+        )
+    ))
+
+
+def write_resp_charges(f: TextIO, charges: List[Charge]) -> None:
+    print(FW("8F10.6").write(charges), file=f)
---
+++ tests/test_resp_charges_util.py
@@ -0,0 +1,32 @@
+from repESP.charges import make_charge
+from repESP.resp_charges_util import parse_resp_charges, write_resp_charges
+
+from my_unittest import TestCase
+
+from io import StringIO
+
+class TestRespCharges(TestCase):
+
+    def setUp(self) -> None:
+        self.charges = list(map(
+            make_charge,
+            [
+                -0.162076,  0.040051,  0.042711,  0.033893, -0.001304, 0.012177,
+                 0.011888,  0.200011, -0.001915, -0.223809,  0.044483, 0.048844,
+                 0.058568, -0.315388,  0.081265,  0.062636,  0.067967
+            ]
+        ))
+
+    def test_parsing(self) -> None:
+        with open("tests/test_resp_charges.qout") as f:
+            parsed_charges = parse_resp_charges(f)
+        self.assertListsAlmostEqual(self.charges, parsed_charges)
+
+    def test_writing(self) -> None:
+
+        written = StringIO()
+        write_resp_charges(written, self.charges)
+        written.seek(0)
+
+        with open("tests/test_resp_charges.qout") as f:
+            self.assertListEqual(f.readlines(), written.readlines())
