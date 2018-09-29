331c38cad6780f44097cbc81cc04d9f7c96859ec
Jan Szopinski <jszopi@gmail.com>
2018-09-29 09:27:19 +0100

    Add averaging function

---
+++ repESP/charge_util.py
@@ -0,0 +1,32 @@
+from .respin_util import Equivalence
+from .charges import Charge, make_charge
+
+import numpy as np
+from typing import Dict, List
+
+
+def average(charges: List[Charge], equivalence: Equivalence) -> List[Charge]:
+
+    if len(charges) != len(equivalence.values):
+        raise ValueError(
+            f"Mismatched lengths of charges ({len(charges)}) and equivalence "
+            f"({len(equivalence.values)}) arguments."
+        )
+
+    equivalent_groups: List[List[int]] = []
+    equivalent_group_of_atom: Dict[int, int] = {}
+
+    for referrer, referred in enumerate(equivalence.values):
+        if referred is not None and referred in equivalent_group_of_atom:
+            equivalent_group = equivalent_group_of_atom[referred]
+            equivalent_groups[equivalent_group].append(referrer)
+            equivalent_group_of_atom[referrer] = equivalent_group
+        else:
+            equivalent_groups.append([referrer])
+            equivalent_group_of_atom[referrer] = len(equivalent_groups) - 1
+
+    return [
+        make_charge(
+            np.mean([charges[j] for j in equivalent_groups[equivalent_group_of_atom[i]]])
+        ) for i in range(len(charges))
+    ]
---
+++ tests/test_charge_util.py
@@ -0,0 +1,45 @@
+from repESP.charge_util import average
+from repESP.charges import Charge, make_charge
+from repESP.respin_util import Equivalence
+
+from my_unittest import TestCase
+
+
+class TestAveraging(TestCase):
+
+    def setUp(self) -> None:
+        self.charges = [make_charge(x) for x in [1, 2, 3, 4, 5, 6]]
+
+    def test_no_equivalence(self) -> None:
+        equivalence = Equivalence([None]*6)
+        result = average(self.charges, equivalence)
+        self.assertListsAlmostEqual(
+            self.charges,
+            result
+        )
+
+    def test_basic_equivalence(self) -> None:
+        equivalence = Equivalence([None, 0] + [None]*4)
+        result = average(self.charges, equivalence)
+        self.assertListsAlmostEqual(
+            [1.5, 1.5, 3, 4, 5, 6],
+            result
+        )
+
+    def test_typical_euqivalence(self) -> None:
+        equivalence = Equivalence([None, None, 0, 1, 0, 1])
+        result = average(self.charges, equivalence)
+        self.assertListsAlmostEqual(
+            [3, 4, 3, 4, 3, 4],
+            result
+        )
+
+    def test_cyclic_euqivalence(self) -> None:
+        # It may be later decided that this should fail Equivalence validation
+        # but until it does, functions in the library should cope with it.
+        equivalence = Equivalence([5, 0, 1, 2, 3, 4])
+        result = average(self.charges, equivalence)
+        self.assertListsAlmostEqual(
+            [3.5]*6,
+            result
+        )
