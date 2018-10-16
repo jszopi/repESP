5df8c3fad73be9b2ab307bcf3abf16da6d9d9bcf
Jan Szopinski <jszopi@gmail.com>
2018-10-16 01:08:35 +0100

    docs: Order members as per source instead of alphabetically

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,8 @@ extensions = [
     'sphinx.ext.githubpages',
 ]
 
+autodoc_member_order = 'bysource'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
