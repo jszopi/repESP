b52473467533958b9f16913d7ab666b6fe94f58a
Jan Szopinski <jszopi@gmail.com>
2018-10-17 22:48:01 +0100

    docs: charges module

--- repESP/charges.py
+++ repESP/charges.py
@@ -10,8 +10,13 @@ from typing import Any, List, Collection, Tuple, Type
 
 
 class Charge(float):
+    """Partial (atomic) charge in units of elementary charge (*e*)
 
-    """Atomic charge [elementary charge]"""
+    Parameters
+    ----------
+    value : Any
+        Any value convertible to float representing the value in units of *e*.
+    """
 
     __slots__ = ()
 
@@ -41,19 +46,44 @@ class Charge(float):
 
 @dataclass
 class AtomWithCharge(Atom):
+    """Dataclass representing an atom with an associated partial charge
+
+    Parameters
+    ----------
+    atomic_number : int
+        The atomic number of the element
+    charge: Charge
+        The partial charge on the atom
+    """
     charge: Charge
 
 
 @dataclass
 class AtomWithCoordsAndCharge(AtomWithCharge, AtomWithCoords):
+    """Dataclass representing an atom in space, with a partial charge
+
+    Parameters
+    ----------
+    atomic_number : int
+        The atomic number of the element
+    charge : Charge
+        The partial charge on the atom
+    coords : Coords
+        The coordinates of the atom in space
+    """
     # NOTE: mypy incorrectly infers the argument order for __init__ to be:
     # (atomic_number, charge, coords), resulting in loads of spurious errors.
     pass
 
 
 class DipoleMomentValue(float):
+    """Dipole moment value in atomic units (:math:`e\mathrm{a}_0`)
 
-    """Dipole moment [bohr * fundamental charge]"""
+    Parameters
+    ----------
+    value : Any
+        Any value convertible to float representing the value in atomic units.
+    """
 
     __slots__ = ()
 
@@ -65,14 +95,31 @@ class DipoleMomentValue(float):
 
 @dataclass
 class DipoleMoment:
+    """Dipole moment vector described through its three components
+
+    Parameters
+    ----------
+    x : DipoleMomentValue
+        The value in the *x* direction
+    y : DipoleMomentValue
+        The value in the *y* direction
+    z : DipoleMomentValue
+        The value in the *z* direction
+    """
+
     x: DipoleMomentValue
     y: DipoleMomentValue
     z: DipoleMomentValue
 
 
 class QuadrupoleMomentValue(float):
+    """Quadrupole moment in atomic units (:math:`e\mathrm{a}_0^2`)
 
-    """Quadrupole moment [bohr^2 * fundamental charge]"""
+    Parameters
+    ----------
+    value : Any
+        Any value convertible to float representing the value in atomic units.
+    """
 
     __slots__ = ()
 
@@ -83,6 +130,24 @@ class QuadrupoleMomentValue(float):
 
 @dataclass
 class QuadrupoleMoment:
+    """Quadrupole moment tensor described through its six components
+
+    Parameters
+    ----------
+    xx : QuadrupoleMomentValue
+        The value in the *xx* direction.
+    yy : QuadrupoleMomentValue
+        The value in the *yy* direction.
+    zz : QuadrupoleMomentValue
+        The value in the *zz* direction.
+    xy : QuadrupoleMomentValue
+        The value in the *xy* direction.
+    xz : QuadrupoleMomentValue
+        The value in the *xz* direction.
+    yz : QuadrupoleMomentValue
+        The value in the *yz* direction.
+    """
+
     xx: QuadrupoleMomentValue
     yy: QuadrupoleMomentValue
     zz: QuadrupoleMomentValue
