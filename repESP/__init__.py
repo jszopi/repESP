4c5fbc5f8844d1e77fd6af90ba940c994683a68e
Jan Szopinski <jszopi@gmail.com>
2018-11-17 15:54:55 +0000

    Add usage caveats to library docs

--- repESP/__init__.py
+++ repESP/__init__.py
@@ -158,4 +158,31 @@ the actual ESP field.
    reader. You should find that the fit quality of your charges is better than
    that of NPA but poorer than that of CHelpG.
 
+Usage caveats
+=============
+
+* The library uses atomic units for all physical properties. If you're dealing with
+  input in other units, you should convert as early in your program as possible.
+  If you require output in other units, you should convert as late as possible.
+* This library attempts to enforce consistent use of values describing physical
+  properties. You shouldn't need to use "naked" `float` values to represent physical
+  properties and should use lightweight classes inheriting from `float` instead.
+  For example, if you mean to specify a bond length of 0.5 a.u., you should use
+  ``types.Dist(0.5)`` and if you mean to specify a charge of -0.1 e, you should
+  use ``charges.Charge(-0.1)``. This aims to ensure that you never try to subtract
+  distance from a charge or try to add values in different units. This area of
+  the library may not be bulletproof and some arithmetic operations may not
+  result in the correct classes and will fall back onto the `float` type,
+  which may cause type checker errors. Note that you will be able to use the
+  inheriting types like `Dist` in any context you can use a `float`.
+* Interacting with this library requires you to use correct types for input
+  arguments. The types are documented and additionally the user should run
+  a static type checker on their script beforehand, for example::
+
+      mypy example.py
+
+  Using a static type checker will save the user debugging tricky runtime
+  errors which tend to occur later in the program. Even if no errors occur, the
+  library may produce erroneous results if the passed in arguments don't follow
+  the documented types.
 """
